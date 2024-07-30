0a0acc7232cc9f7165ac9e02e8533c0267419083
green1052 <green1052@pm.me>
2024-07-31 04:10:53 +0900

    index.tsx 업데이트

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import App from "./App.jsx";
 import {createTheme, ThemeProvider} from "@mui/material/styles";
 import {createRoot} from "react-dom/client";
+import CssBaseline from "@mui/material/CssBaseline";
 
 const darkTheme = createTheme({
     palette: {
@@ -14,6 +15,7 @@ const root = createRoot(container!);
 
 root.render(
     <React.StrictMode>
+        <CssBaseline/>
         <ThemeProvider theme={darkTheme}>
             <App/>
         </ThemeProvider>
